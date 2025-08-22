5cfbaa217f62af5b485be1b16243b1a71962e6f4
JacobNYJC <jacob.poh.jing.hang_2025@nyjc.edu.sg>
2025-08-22 07:00:35 +0000

    a

---
+++ text.py
@@ -0,0 +1,22 @@
+room_types = ['treasure', 'monster']
+
+maze_size = 15
+
+welcome_prompt = "Welcome to Labyrinth Ascension, a Single Player MUD game. Do you want to start or load your game?"
+start_choices = ['start', 'quit', 'load']
+started_text = 'To choose an option, type out the entire option.'
+
+input_prompt = "Please select a choice: "
+input_error_prompt = "Please input again."
+
+combat_sequence_prompt = "Here are your available moves: "
+ability_addition_error = "Please input a valid ability name"
+magnitude_prompt = "Please input the magnitude of ability: "
+cheapest_ability_cost = 1
+
+successful_room_travel = "You have travelled to room with ID: "
+thanks_message = "Thanks for playing!"
+
+directions = ['up', 'down', 'left', 'right']
+
+printing_text_spacing = '\n=================== \n'
